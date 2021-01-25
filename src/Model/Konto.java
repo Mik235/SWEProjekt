3410f69cf72cd8f37dfc10be745cfd86e4139cc5
Lukas <Lukas.Hertzberg@gmx,de>
2021-01-25 11:16:09 +0100

    Methode buchen erstellt

--- src/Model/Konto.java
+++ src/Model/Konto.java
@@ -1,4 +1,44 @@
 package Model;
 
-public class Konto {
+import java.text.DecimalFormat;
+
+public abstract class Konto {
+    private int kontoNr;
+    private double kontoStand;
+    private String kontoTyp;
+
+    public Konto(int kontoNr, double kontoStand, String kontoTyp) {
+        this.kontoNr = kontoNr;
+        this.kontoStand = kontoStand;
+        this.kontoTyp = kontoTyp;
+    }
+    
+    
+    public void buchen(double p_buchen){
+        kontoStand += p_buchen;
+    }
+
+    public int getKontoNr() {
+        return kontoNr;
+    }
+
+    public void setKontoNr(int kontoNr) {
+        this.kontoNr = kontoNr;
+    }
+
+    public double getKontoStand() {
+        return kontoStand;
+    }
+
+    public void setKontoStand(double kontoStand) {
+        this.kontoStand = kontoStand;
+    }
+
+    public String getKontoTyp() {
+        return kontoTyp;
+    }
+
+    public void setKontoTyp(String kontoTyp) {
+        this.kontoTyp = kontoTyp;
+    }
 }
