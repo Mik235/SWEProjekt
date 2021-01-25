afa9308f2d3ff779c49ad37056a68e53c7c307a3
Mika <mikaaust@gmx.de>
2021-01-25 10:26:15 +0100

    Event Finished

--- src/Model/event.java
+++ src/Model/event.java
@@ -1,4 +1,51 @@
 package Model;
 
+import java.util.Date;
+
 public class event {
+    int datiZeilnr;
+    String sachbekürz;
+    int Kontonr;
+    double Kontostandvdb;
+    double Buchungsbetrag;
+    double Kontostandndb;
+    Date Buchungsdatum;
+
+    public event(int datiZeilnr, String sachbekürz, int kontonr, double kontostandvdb, double buchungsbetrag, double kontostandndb) {
+        this.datiZeilnr = datiZeilnr;
+        this.sachbekürz = sachbekürz;
+        Kontonr = kontonr;
+        Kontostandvdb = kontostandvdb;
+        Buchungsbetrag = buchungsbetrag;
+        Kontostandndb = kontostandndb;
+        Buchungsdatum=new Date();
+    }
+
+    public int getDatiZeilnr() {
+        return datiZeilnr;
+    }
+
+    public String getSachbekürz() {
+        return sachbekürz;
+    }
+
+    public int getKontonr() {
+        return Kontonr;
+    }
+
+    public double getKontostandvdb() {
+        return Kontostandvdb;
+    }
+
+    public double getBuchungsbetrag() {
+        return Buchungsbetrag;
+    }
+
+    public double getKontostandndb() {
+        return Kontostandndb;
+    }
+
+    public Date getBuchungsdatum() {
+        return Buchungsdatum;
+    }
 }
