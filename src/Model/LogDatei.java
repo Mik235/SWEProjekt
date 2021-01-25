e074c177996109ad496088f8399c52f0c3bee175
Mika <mikaaust@gmx.de>
2021-01-25 10:32:30 +0100

    Finished Log

--- src/Model/LogDatei.java
+++ src/Model/LogDatei.java
@@ -1,7 +1,19 @@
 package Model;
 
-public class LogDatei {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class LogDatei implements Serializable {
+    ArrayList<event> events=new ArrayList<>();
     public LogDatei() {
-        System.out.println("TEST");
+
+    }
+    public void addEintrag(String sachbekürz, int kontonr, double kontostandvdb, double buchungsbetrag, double kontostandndb){
+        events.add(new event(events.size(),sachbekürz,kontonr,kontostandvdb,buchungsbetrag,kontostandndb));
+
+    }
+
+    public ArrayList<event> getEvents() {
+        return events;
     }
 }
