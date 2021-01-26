39df17a0da02d07775f790892de82fbebce8f3b3
lms <lim>
2021-01-26 11:04:41 +0100

    txtSchreiben in LogDatei

--- src/Model/LogDatei.java
+++ src/Model/LogDatei.java
@@ -1,5 +1,8 @@
 package Model;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -11,7 +14,19 @@ public class LogDatei implements Serializable {
     public void addEintrag(String sachbekuerz, int kontonr, double kontostandvdb, double buchungsbetrag, double kontostandndb){
         events.add(new Event(events.size(),sachbekuerz,kontonr,kontostandvdb,buchungsbetrag,kontostandndb));
 
+        String einfuegen = sachbekuerz + "" + kontonr + " "+ kontostandvdb + " " + buchungsbetrag + " " + kontostandndb;
+        txtschreiben(einfuegen);
     }
+
+    private void txtschreiben(String einfuegen) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("logDatei.txt", true))) {
+            bw.write(String.valueOf(einfuegen + "\n"));
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public ArrayList<Event> getEvents() {
         return events;
