9cc38ef01b4804b99b28085e40c8ba5670882332
lms <lim>
2021-01-26 10:21:44 +0100

    Aenderung von Konto auf Bankingm

--- src/Model/Bankingm.java
+++ src/Model/Bankingm.java
@@ -1,4 +1,16 @@
 package Model;
 
 public class Bankingm {
+
+    public Bankingm() {
+        LogDatei log;
+    }
+
+    public Konto selectKonto(String pKontonr ){
+        return null;
+    }
+
+    public void buchenKonto(String pKontonr, String pbetrag){
+
+    }
 }
--- src/Model/Konto.java
+++ src/Model/Konto.java
@@ -2,17 +2,6 @@ package Model;
 
 public abstract class Konto {
 
-    public Konto() {
-        LogDatei log;
-    }
-
-    public Konto selectKonto(String pKontonr ){
-        return null;
-    }
-
-    public void buchenKonto(String pKontonr, String pbetrag){
-
-    }
 
 
 }
