07009a7ba027ceb21db621ad36d8d0c8c1562d99
lms <lim>
2021-01-26 10:15:49 +0100

    abstracte Klasse Konto

--- src/Model/Konto.java
+++ src/Model/Konto.java
@@ -1,4 +1,18 @@
 package Model;
 
 public abstract class Konto {
+
+    public Konto() {
+        LogDatei log;
+    }
+
+    public Konto selectKonto(String pKontonr ){
+        return null;
+    }
+
+    public void buchenKonto(String pKontonr, String pbetrag){
+
+    }
+
+
 }
