feb97d1955f36c1244bf4b557cce7de257936c22
Mika <mikaaust@gmx.de>
2021-01-26 10:15:42 +0100

    Interaktion Controller->View

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -3,13 +3,27 @@ package Controller;
 import Model.Bankingm;
 import View.View;
 
-public class Controller {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controller implements ActionListener {
     View v;
     Bankingm m;
 
-    public Controller() {
+    public Controller () {
         v=new View();
         m=new Bankingm();
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("rdy")){
+            System.out.println("Es wurde ok gedrückt");
+
+        }
+        if(e.getActionCommand().equals("Abbrechen")){
+            System.out.println("Abbrechen");
+        }
     }
 }
