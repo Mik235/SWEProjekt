be9e6757bb9cbcbcc2801dc467b3fa9ba0152b67
Mika <mikaaust@gmx.de>
2021-01-26 11:16:05 +0100

    Finished Banking Modell

--- src/Model/BankingModell.java
+++ src/Model/BankingModell.java
@@ -6,5 +6,14 @@ public class BankingModell {
     ArrayList<KontomitLog> konten = new ArrayList<>();
 
     public BankingModell() {
+
+    }
+    public void addkonto(int typ){
+        if(typ==0){
+           konten.add(new KontomitLog(new SparkKonto(konten.size(),0,"Sparkonto")));
+        }
+        else if (typ==1){
+            konten.add(new KontomitLog(new GiroKonto(konten.size(),0,"Girokonto")));
+        }
     }
 }
